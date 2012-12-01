76527bb9065fb11bbe6af11e6192929769709926
Jason French <frenchja@gmail.com>
2012-11-30 19:59:27 -0600

    Initial Commit for stream.sh

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-north-american-playground
-=========================
+# README
+This is a small script that I use stream video content to friends.  I originally wrote it in BASH, although I will soon port it to Python to for Windows users.  Even so, I'll try as best as possible to debug and add features to the BASH version as it's a fairly quick and painless language for me to write in.
+
+## Instructions for stream.sh
+Install the following programs:
+
+  - [VLC](http://www.videolan.org/index.html)
+  - [oggfwd](http://v2v.cc/~j/oggfwd/)
+  - [ffmpeg2theora](http://firefogg.org/nightly/)
+  - [Icecast](http://www.icecast.org/)
+
+### Mac OS X Notes
+Some of these programs can be installed easily on OS X with [Macports](https://www.macports.org/). Others, such as ffmpeg2theora, can be moved directly from the ~/Downloads folder to /usr/local/bin/ or /opt/local/bin/ using `mv ffmpeg2theora.macosx /opt/local/bin/`.  
+
+`oggfwd` can be easily compiled on OS X by changing a few parameters in the `make` file.  First, install `libshout`, `libtheora`, `libogg`, and `libvorbis` via MacPorts.  Then, simply change the prefix for the library from `/usr/` to `/opt/local/`.  This should built a binary that can be dropped in /opt/local/bin just as ffmpeg2theora.macosx was.
+
+### Linux
+I haven't directly installed the prerequisites under Linux yet, but I'm sure most can be found using `apt-cache search` or `yum search`, depending on whether you're using Ubuntu or Fedora, respectively.
+
+## Configuration
+
+First, set the password on line 72 to match whatever is in your icecast.xml file. Second, you may want to edit the website on line 70 to match your own. 
+
+## Features to Add
+  - More robust error handling.
+  - Playlist feature for ffmpeg2theora option in BASH.
+  - Utilize VLC's [web interface](http://wiki.videolan.org/Documentation:Modules/http_intf).
+  - Video codec choices (e.g., [Theora](http://www.theora.org/) vs. [x264](http://www.videolan.org/developers/x264.html) vs. [VP8](http://www.webmproject.org/))
---
+++ stream.sh
@@ -0,0 +1,109 @@
+#!/bin/bash
+# Install List:
+# 	ffmpeg2theora:  http://firefogg.org/nightly/
+# 	icecast:  http://www.icecast.org/download.php
+# 	oggfwd:  http://v2v.cc/~j/oggfwd/
+
+# Works under Mac OS X & Linux
+
+#[ "$#" -eq 1 ] || echo "You must provide a movie parameter after stream.sh!" && exit 0
+
+MOVIE=$1
+HOSTNAME="$(hostname)"
+clear
+echo "Your hostname appears to be $(hostname)."
+echo "Users will need to connect to this."
+echo
+read -p "Is this correct? " yn
+case $yn in
+	Y|y|Yes|yes)
+		break ;;
+	*)
+		read -p "Enter your hostname: " hostname;;
+esac
+
+function vlc (){
+	if "$(uname -s)" == 'Darwin':
+		VLC='/Applications/VLC.app/Contents/MacOS/VLC'
+	else:
+		VLC="$(type -P vlc)"
+	VLC "$MOVIE" \
+		--sout="#transcode{vcodec=x264{bframes=5},vb=256,scale=0.67,acodec=aac,ab=32,channels=1,audio-sync=1,fps=15,}:standard{access=http,mux=ts,dst='http://$HOSTNAME:8000'}"
+
+	vlcid=$!
+	wait
+	kill $vlcid
+	pkill vlc
+
+	exit 1
+
+}
+
+function ffmpeg (){
+	clear
+	echo "================================="
+	echo "Playing $(basename "$MOVIE") at:"
+	echo "http:$HOSTNAME:8000/mst3k.ogg"
+	echo
+	echo "Press Ctrl+c to exit at any time."
+	echo "================================="
+	echo
+	ICECAST="$(type -P icecast)"
+	$ICECAST -b -c /opt/local/etc/icecast.xml &
+	icepid=$!
+
+	FFMPEG="$(type -P ffmpeg2theora.macosx)"
+	OGGFWD="$(type -P oggfwd)"
+
+	"$FFMPEG" "$MOVIE" \
+		-a 0 \
+		-v 6 \
+		--max_size "640x480" \
+		-c 1 \
+		-F 15 \
+		--no-skeleton \
+		--subtitle-types=all \
+		--subtitles-language "en_US" \
+		--artist "$(users)" \
+		--title "$(basename "$MOVIE")" \
+		--date "$(date +%Y-%m-%d)" \
+		--organization "http://mst3k.nofadz.com/chat/" \
+		-o /dev/stdout - | \
+		$OGGFWD $(hostname) 8000 password /mst3k.ogg
+	oggpid=$!
+	
+	wait
+	kill $icepid
+	pkill icecast
+	
+	exit 1
+}
+
+menu (){
+	clear
+	echo "There are two ways of streaming:"
+	echo "1)  Using Icecast, ffmpeg2theora, and oggfwd."
+	echo "2)  Using VLC."
+	echo ""
+	echo "Q)  Quit."
+	echo ""
+	read -p "Which would you like to try? " s
+
+	case "$s" in
+		1 )
+			ffmpeg ;;
+		2 )
+			read -p "Not implemented yet.  Choose 1!"
+			menu ;;
+		Q|q|Quit|quit )
+			exit 1 ;;
+		* )
+			echo "Invalid choice!"
+                        	menu ;;
+	esac
+
+	wait 
+	exit 1
+}
+
+menu
